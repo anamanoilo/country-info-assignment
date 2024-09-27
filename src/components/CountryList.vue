556c9dd41d6767f47cedb4730299de085358d360
Anastasiia Manoilo <anastasiamanoilo@gmail.com>
2024-09-27 20:29:53 +0300

    feat: create components for the HomeView

---
+++ src/components/CountryList.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+  
+interface Country {
+  name: string;
+  countryCode: string;
+}
+
+
+  const searchQuery = ref('');
+  const countries = ref<Country[]>([]);
+
+
+</script>
+
+<template>
+  <div>
+      <input
+      type="text"
+        v-model="searchQuery"
+        class="border p-2 rounded w-full"
+        placeholder="Search for a country..."
+      />
+  <h3>Countries List</h3>
+  <ul v-if="countries.length" class="mt-4">
+     <li
+          v-for="country in countries"
+          :key="country.countryCode"
+          class="cursor-pointer hover:underline"
+        >
+          {{ country.name }}
+        </li>
+  </ul>
+</div>
+</template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,55 @@
-<script>
-export default {
-  methods: {
-    goToAbout() {
-      this.$router.push('/country')
-    },
-  },
-}
+<script setup lang="ts">
+
+// const countries = [{
+// "countryCode": "AD",
+// "name": "Andorra"
+// },
+// {
+// "countryCode": "AL",
+// "name": "Albania"
+// },
+// {
+// "countryCode": "AM",
+// "name": "Armenia"
+// },
+// {
+// "countryCode": "AR",
+// "name": "Argentina"
+// },
+// {
+// "countryCode": "AT",
+// "name": "Austria"
+// },
+// {
+// "countryCode": "AU",
+// "name": "Australia"
+// },
+// {
+// "countryCode": "AX",
+// "name": "Åland Islands"
+// },
+// {
+// "countryCode": "BA",
+// "name": "Bosnia and Herzegovina"
+// },
+// {
+// "countryCode": "BB",
+// "name": "Barbados"
+// }]
+// export default {
+//   methods: {
+//     goToAbout() {
+//       this.$router.push('/country')
+//     },
+//   },
+// }
+
+
+
 </script>
 
 <template>
-  <h2>HomeView</h2>
-  <button @click="goToAbout">Go to About</button>
+  <div class="w-full px-4 mx-auto sm:w-[450px] md:w-[748px] lg:w-[1250px] border-2 border-green-500"><h2>HomeView</h2></div>
+  
+  <!-- <button @click="goToAbout">Go to About</button> -->
 </template>
